262939844e016b38a7fe38571d3cac8d96471f5b
Anuj Gupta <anujgupta394@gmail.com>
2026-09-05 22:40:43 +0530

    Separate telemetry core CMake configuration

---
+++ build/Qt_6_11_1_for_macOS_Debug/CMakeFiles/network_simulator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/network_simulator.dir/apps/network_simulator/main.cpp.o"
+  "CMakeFiles/network_simulator.dir/apps/network_simulator/main.cpp.o.d"
+  "network_simulator"
+  "network_simulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/network_simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
