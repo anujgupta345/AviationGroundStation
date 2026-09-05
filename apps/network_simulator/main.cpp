3b20c28e66394fd6e086559bdb04982b7bcd445e
Anuj Gupta <anujgupta394@gmail.com>
2026-09-05 21:35:22 +0530

    Add shared telemetry core

---
+++ apps/network_simulator/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "AircraftTelemetry.h"
+
+int main()
+{
+    AircraftTelemetry telemetry{
+        101,
+        27.1767,
+        78.0081,
+        12000.0,
+        450.0
+    };
+
+    std::cout << "Network Simulator started\n\n";
+
+    std::cout << "Aircraft ID : " << telemetry.aircraftId << '\n';
+    std::cout << "Latitude    : " << telemetry.latitude << '\n';
+    std::cout << "Longitude   : " << telemetry.longitude << '\n';
+    std::cout << "Altitude    : " << telemetry.altitude << " m\n";
+    std::cout << "Speed       : " << telemetry.speed << " km/h\n";
+
+    return 0;
+}
---
+++ libs/telemetry_core/include/AircraftTelemetry.h
@@ -0,0 +1,14 @@
+#ifndef AIRCRAFTTELEMETRY_H
+#define AIRCRAFTTELEMETRY_H
+
+
+struct AircraftTelemetry
+{
+    int aircraftId;
+    double latitude;
+    double longitude;
+    double altitude;
+    double speed;
+};
+
+#endif // AIRCRAFTTELEMETRY_H
